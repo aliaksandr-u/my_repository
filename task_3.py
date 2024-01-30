78527180e11d58d5508668e49648b79238bf513f
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-01-30 21:59:33 +0300

    task_3

--- task_3.py
+++ task_3.py
@@ -5,3 +5,9 @@ age = input("Введите возраст: ")
 city = input("Введите город: ")
 text = "Привет %s %s из %s" % (name, age, city)
 print(text)
+
+text2 = "Привет {} {} из {}".format(name, age, city)
+print(text2)
+
+text3 = f"Привет {name} {age} из {city}"
+print(text3)
