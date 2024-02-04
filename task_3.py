9cac45c5ba39350e4f2057a7bee91def7d023e76
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-02-04 20:27:48 +0300

    lesson_4 task_3

--- task_3.py
+++ task_3.py
@@ -1,6 +1,11 @@
 #*Заполнить словарь где ключами будут выступать числа от 0 до n, а
 #значениями вложенный словарь с ключами "name" и "email", а значения
 #для этих ключей будут браться с клавиатуры
+user_email = {}
 n = int(input("Введите число: "))
-name = input("Введите имя: ")
-email = input("Введите email: ")
+for i in range(0, n+1):
+    name = input("Введите имя: ")
+    email = input("Введите email: ")
+    a = {name: email}
+    user_email [i] = a
+print(user_email)
