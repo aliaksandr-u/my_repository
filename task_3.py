e21584462f2b97cca484db4639132f8ed41a6b11
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-02-10 16:09:22 +0300

    lesson_5

--- task_3.py
+++ task_3.py
@@ -1,14 +1,1 @@
-#*Заполнить словарь где ключами будут выступать числа от 0 до n, а
-#значениями вложенный словарь с ключами "name" и "email", а значения
-#для этих ключей будут браться с клавиатуры
-#user_email = {}
-#n = int(input("Введите число: "))
-#for i in range(0, n+1):
-#    name = input("Введите имя: ")
-#    email = input("Введите email: ")
-#    a = {name: email}
-#    user_email[i] = a
-#print(user_email)
-
-data = {i: {"name": input(f"name {i}: "), "email": input(f"email {i}: ")} for i in range(3)}
-print(data)
+#Вывести четные числа от 2 до N по 5 в строку
