8c35e262dca59d872872d9e3a815c82a779899ac
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-02-11 14:15:46 +0300

    lesson_5 task_3

--- task_3.py
+++ task_3.py
@@ -1,9 +1,5 @@
 #Вывести четные числа от 2 до N по 5 в строку
 N = int(input("N: "))
 i = 2
-while i <= N:
-    if i % 2 ==0:
-        print(i)
-        i = i +1
-    else:
-        i = i +1
+for i in range(2, N+1, 2):
+    print(i, end=" ")
