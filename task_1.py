a60254b386e0517c39882a8a7341c468e7f4a55f
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-02-11 13:41:12 +0300

    lesson_5 task_1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,13 @@
 #Вывести первые N цисел кратные M и больше K
+N = int(input("N: "))
+M = int(input("M: "))
+K = int(input("K: "))
+i = 1
+j = 1
+while i <= N:
+    if j * M > K:
+        print(j * M)
+        i = i + 1
+        j = j + 1
+    else:
+        j = j + 1
