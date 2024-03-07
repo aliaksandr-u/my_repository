2195a9fac7d4f833351a4e226d53669771cd454f
aliaksandr-u <aliaksandr.urban@yandex.by>
2024-03-07 20:00:26 +0300

    lesson_13

--- lesson_13.py
+++ lesson_13.py
@@ -1,184 +1,180 @@
-# # from psycopg2.extras import NamedTupleConnection, NamedTupleCursor
-# #
-# # with NamedTupleConnection(dsn="postgres://admin:admin@217.76.60.77:6666/admin") as connection:
-# #     with connection.cursor() as cursor:  # type: NamedTupleCursor
-#         # cursor.execute("""
-#         #     CREATE TABLE IF NOT EXISTS departments (
-#         #         id SERIAL PRIMARY KEY ,
-#         #         name VARCHAR ( 32 ) NOT NULL UNIQUE
-#         #     );
-#         # """)
-#         # cursor.execute("""
-#         #     CREATE TABLE IF NOT EXISTS sub_departments (
-#         #         id SERIAL PRIMARY KEY ,
-#         #         name VARCHAR ( 32 ) NOT NULL UNIQUE
-#         #     );
-#         # """)
-#         # cursor.execute("""
-#         #     CREATE TABLE IF NOT EXISTS users (
-#         #         id SERIAL PRIMARY KEY ,
-#         #         department_id INTEGER ,
-#         #         sub_department_id INTEGER,
-#         #         FOREIGN KEY (department_id) REFERENCES departments(id),
-#         #         FOREIGN KEY (sub_department_id) REFERENCES sub_departments(id)
-#         #     );
-#         # """)
-#         # cursor.execute("""
-#         #     CREATE TABLE IF NOT EXISTS chats (
-#         #         id SERIAL PRIMARY KEY ,
-#         #         name VARCHAR ( 32 ) NOT NULL UNIQUE
-#         #     );
-#         # """)
-#         # cursor.execute("""
-#         #     CREATE TABLE IF NOT EXISTS chat_relations (
-#         #         id SERIAL PRIMARY KEY ,
-#         #         chat_id INTEGER NOT NULL ,
-#         #         department_id INTEGER ,
-#         #         sub_department_id INTEGER,
-#         #         FOREIGN KEY (department_id) REFERENCES departments(id),
-#         #         FOREIGN KEY (sub_department_id) REFERENCES sub_departments(id),
-#         #         FOREIGN KEY (chat_id) REFERENCES chats(id)
-#         #     );
-#         # """)
-#         # connection.commit()
-#         # cursor.execute(query="""
-#         #     SELECT chats.name FROM chat_relations
-#         #     JOIN chats ON chats.id  = chat_relations.chat_id
-#         #     WHERE (
-#         #         chat_relations.department_id IS NULL OR
-#         #         chat_relations.department_id = (
-#         #             SELECT users.department_id FROM users WHERE users.id = %(user_id)s
-#         #         )
-#         #     ) AND (
-#         #         chat_relations.sub_department_id IS NULL OR
-#         #         chat_relations.sub_department_id = (
-#         #             SELECT users.sub_department_id FROM users WHERE users.id = %(user_id)s
-#         #         )
-#         #     );
-#         # """, vars={"user_id": 3})
-#         # print(cursor.fetchall())
-# from redis import Redis
-# from sqlalchemy import select, update, delete, insert, and_, all_, or_, any_, func, Table, alias
-# from sqlalchemy.orm import selectinload, joinedload
-# from lesson11 import *
+# from enum import IntEnum
 #
+# from sqlalchemy import Column, INT, TIMESTAMP, Enum, ForeignKey, create_engine, select, insert
+# from sqlalchemy.orm import DeclarativeBase, sessionmaker, relationship, joinedload
+# from sqlalchemy.sql import func
+#
+#
+# class TaskStatus(IntEnum):
+#     TODO: int = 1
+#     IN_PROGRESS: int = 2
+#     DONE: int = 3
+#
+#
+# class Base(DeclarativeBase):
+#     ...
+#
+#
+# class Project(Base):
+#     __tablename__ = "project"
+#
+#     id = Column(INT, primary_key=True)
+#
+#
+# class User(Base):
+#     __tablename__ = "user"
+#
+#     id = Column(INT, primary_key=True)
+#
+#
+# class Task(Base):
+#     __tablename__ = "task"
+#
+#     id = Column(INT, primary_key=True)
+#     author_id = Column(INT, ForeignKey("user.id"), nullable=False)
+#     executor_id = Column(INT, ForeignKey("user.id"), nullable=False)
+#     start_date = Column(TIMESTAMP, nullable=False)
+#     end_date = Column(TIMESTAMP, nullable=False)
+#     status = Column(Enum(TaskStatus), nullable=False, default=TaskStatus.TODO)
+#
+#
+# engine = create_engine("sqlite:///db.db")
+#
+# stmt = select(Task, User).join(User, Task.executor_id == User.id)
+# stmt = stmt.filter(Task.status != TaskStatus.DONE)
+# stmt = stmt.filter(Task.end_date < func.now())
+#
+# session_maker = sessionmaker(bind=engine)
 #
 # # with session_maker() as session:
-#     # session.scalars()
-#     # session.execute().scalars()
-#     # session.scalar()
-#     # session.execute().scalar()
-#
-# # stmt = select(Topic)
-# # stmt = stmt.order_by(Topic.date_created.desc())
-# # stmt = stmt.order_by(Topic.title)
-# # stmt = stmt.filter(or_(Topic.id >= 2, ~Topic.title.icontains("new")))
-# # # stmt = stmt.filter_by(id=5, title="sport")
-# # print(stmt)
-# user_id = 1
-# q = select(Chat).join(ChatRelation)
-# q = q.filter(
-#     and_(
-#         or_(
-#             ChatRelation.department_id.is_(None),
-#             ChatRelation.department_id == (
-#                 select(User.department_id).filter(User.id == user_id)
-#             )
-#         ),
-#         or_(
-#             ChatRelation.sub_department_id.is_(None),
-#             ChatRelation.sub_department_id == (
-#                 select(User.sub_department_id).filter(User.id == user_id)
-#             )
-#         ),
-#     )
-# )
-# # stmt = select(Topic.id + 5).filter(func.max(Topic.id))
-# # print(stmt)
-# # with session_maker() as session:
-# #     response = session.scalars(statement=q)
-# #     print(response.unique().all())
-#
-# # stmt = update(Topic).values(title="qwerqwer")
-# # stmt = insert(Tag).values()
-#
-#
-# # TopicTag = Table(
-# #     "topic_tags",
-# #     Base.metadata,
-# #     Column("topic_id", INT, ForeignKey("topics.id"), primary_key=True),
-# #     Column("tag_id", INT, ForeignKey("tags.id"), primary_key=True),
-# # )
-# # TopicTag.insert().values([{}, {}, {}])
-#
-# # q1 = select(TopicTag.c.topic_id).filter(TopicTag.c.tag_id == 5)
-# # print(q1)
-#
-# # with session_maker() as session:
-# #     # user = session.get(User, 1, options=[joinedload(User.department, User.sub_department)])
-# #     user = session.scalar(
-# #         select(User)
-# #         .options(
-# #             joinedload(User.department).subqueryload(Department.chats),
-# #             joinedload(User.sub_department)
-# #         )
+# #     objs = session.scalars(select(User)).all()
+# #     print(objs)
+# #     session.execute(
+# #         insert(User).values([{"id": 1}, {"id": 2}, {"id": 3}])
 # #     )
-# #     print(user.department.chats)
-#
-#
-# from pydantic import BaseModel, PositiveInt, Field
-#
-#
-# class DepartmentDetail(BaseModel):
-#     id: PositiveInt
-#     name: str = Field(default=..., max_length=32)
-#
-#
-# class SubDepartmentDetail(BaseModel):
-#     id: PositiveInt
-#     name: str = Field(default=..., max_length=32)
-#
-#
-# class ChatDetail(BaseModel):
-#     id: PositiveInt
-#     name: str = Field(default=..., max_length=32)
-#     departments: list[DepartmentDetail]
-#     sub_departments: list[SubDepartmentDetail]
-#
-#
-# # with session_maker() as session:
-# #     chat = session.get(Chat, 1, options=[joinedload(Chat.departments), joinedload(Chat.sub_departments)])
-# #     chat_detail = ChatDetail.model_validate(obj=chat, from_attributes=True)
-# #     print(chat_detail)
-#
-#
-# from datetime import timedelta
+# #     session.commit()
+# from random import randint
+# from threading import *
 # from time import sleep
+# from queue import Queue, LifoQueue, PriorityQueue
 #
-# from redis import Redis
-# from json import dumps, loads
 #
-# redis = Redis.from_url(url="redis://0.0.0.0:6379/0")
-# print(redis.keys("*u*"))
-# redis.set(name="user", value=dumps({"key": "value"}))
-# print(loads(redis.get("user")))
-# redis.lpush("myqueue", 1, 2, 3, 4)
-# print(redis.rpop("myqueue"))
-# print(redis.rpop("myqueue"))
-# redis.lpush("myqueue", "HELLO")
-# print(redis.llen("myqueue"))
-# redis.set("session", "qwerqwer", ex=timedelta(seconds=10))
-# print(redis.get("session"))
-# sleep(5)
-# print(redis.get("session"))
-# sleep(5)
-# print(redis.get("session"))
-from enum import IntEnum
+# print_lock = RLock()
+# s = Semaphore(value=3)
+# b = Barrier(parties=3)
+# e = Event()
+# l = local()
+# q = PriorityQueue()
 
-from sqlalchemy import Column, Enum
+# def ping():
+#     # b.wait(timeout=1)
+#     for _ in range(10):
+#         with print_lock:
+#             print(current_thread().name)
+#         sleep(1)
+#
+#
+# def pong():
+#     # b.wait(timeout=1)
+#     for _ in range(10):
+#         with print_lock:
+#             print(current_thread().name)
+#         sleep(1)
 
 
-class Status(IntEnum):
-    TO_DO = 0
-    IN_PROGRESS = 1
-    DONE = 2
+# threads = [Thread(target=ping if i % 2 else pong) for i in range(10)]
+# for thread in threads:
+#     thread.start()
+
+# def foo():
+#     for _ in range(10):
+#         obj = (randint(1, 10), f"{randint(1, 10)}")
+#         sleep(0.5)
+#         print(f"{obj=}")
+#         q.put(obj)
+#
+#
+# def bar():
+#     for _ in range(10):
+#         sleep(1)
+#         print(q.get())
+#
+#
+# t1 = Thread(target=foo)
+# t2 = Thread(target=bar)
+# t1.start()
+# t2.start()
+# from multiprocessing import Process
+#
+#
+# def bar():
+#     print("BAR")
+#
+#
+# def foo():
+#     p = Process(target=bar)
+#     p.start()
+#
+#
+# if __name__ == '__main__':
+#     # foo()
+#     p = Process(target=foo)
+#     p.start()
+from asyncio import create_task, run, current_task, sleep
+
+
+async def foo():
+    for _ in range(10):
+        print(current_task().get_name())
+
+
+async def bar():
+    tasks = [create_task(foo()) for _ in range(10)]
+    for task in tasks:
+        await task
+
+
+class AsyncIterator:
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        return "async"
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        ...
+
+
+# async def main():
+#     async with AsyncIterator() as ai:
+#         ...
+    # async for i in AsyncIterator():
+    #     print(i)
+
+
+def wrapper(func):
+    async def wrapped(*args, **kwargs):
+        return await func(*args, **kwargs)
+
+    return wrapped
+
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+# async_engine = create_async_engine("postgresql+asyncpg://admin:admin:0.0.0.0:5432/admin")
+async_engine = create_async_engine("sqlite+aiosqlite:///db.db")
+async_session_maker = async_sessionmaker(bind=async_engine)
+
+
+# async def main():
+#     async with async_session_maker() as session:
+#         session.add()
+#         await session.commit()
+#         session.execute()
+#         session.scalars()
+#         session.scalar()
